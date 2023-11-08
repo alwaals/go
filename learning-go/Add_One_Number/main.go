758dd3612bacfaaf3254dbec64106632f907710a
“Swetha <swetha.alwaal@gmail.com>
2023-11-07 21:10:36 -0600

    new changes_Nov_7_2023

---
+++ learning-go/Add_One_Number/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := []int{1, 8, 9,9,9}
+	fmt.Println(addNumber(i))
+}
+func addNumber(inp []int) []int {
+	borrow:=false
+	if len(inp)>1{
+	for i := len(inp) - 1; i > 0; i-- {
+		if inp[i]+1 < 10 && !borrow{
+			inp[i] = inp[i] + 1
+			break
+		} else if !borrow{
+			inp[i] = 0
+			if inp[i]+1 < 10 {
+				inp[i-1] = inp[i-1] + 1
+				if inp[i-1]>9{
+					borrow=true
+					continue
+				}
+				break
+			} else {
+				borrow=true
+			}
+		} else {
+			if inp[i] < 10{
+				break
+			}else {
+				inp[i] = 0
+				inp[i-1] = inp[i-1] + 1
+				borrow=true
+			}
+		}
+	}
+}else{
+	inp[0]=inp[0]+1
+}
+	return inp
+}
+func addition(inp []int) {
+}
---
+++ learning-go/rotate_array/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+i:=[]int{1, 2, 3, 4, 5}
+fmt.Println(rotate(i,3))
+}
+
+func rotate(inp []int,b int)[]int{
+	var res []int
+	// lastEle := inp[b:]
+	// res = inp[0:len(lastEle)]
+	// fmt.Println(lastEle,res)
+
+	secondHalf := inp[0:len(inp)-b]
+	res=inp[len(inp)-b:]
+	//fmt.Println(secondHalf,res)
+	res= append(res, secondHalf...)
+	return res
+}
